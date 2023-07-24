8c5ec5152ab43a1825ceb0721890667cb0159dcd
ARO <lepridev35@gmail.com>
2023-07-24 14:17:49 +0000

    modification du WorkingStep

--- src/ui/design-system/steps/WorkingStep.tsx
+++ src/ui/design-system/steps/WorkingStep.tsx
@@ -15,7 +15,7 @@ interface Props {
 
 const WorkingStep = ({ icon, label, description, color, className }: Props) => {
   return (
-    <div className="flex flex-col items-center justify-between p-3 bg-white shadow-md max-w-sm">
+    <div className="flex flex-col items-center justify-between p-3 bg-white shadow-md max-w-sm rounded-md">
       <div
         className={clsx(
           "flex items-center justify-center p-5 mb-6 w-[80px] h-[80px] rounded-xl rotate-45 border-2",
